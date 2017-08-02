69840a7de27bfc509f15204334cb1a258e691cbf
cqHyp <752905517@qq.com>
2017-08-02 16:02:55 +0800

    all_commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <img src="./common/img/gakki.jpg"/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+};
+</script>
+
+<style>
+
+</style>
